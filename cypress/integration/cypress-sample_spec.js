f966074f096725f74ea4113b2a88263bed6820a1
Jess Bring-Larsen <jess.bring-larsen@teliacompany.com>
2019-06-07 16:15:17 +0200

    Added beforeEach example

--- cypress/integration/cypress-sample_spec.js
+++ cypress/integration/cypress-sample_spec.js
@@ -5,12 +5,17 @@ describe('Sample Cypress.io tests', function() {
   })
   
   describe('example.cypress.io tests', function() {
+    beforeEach(() => {
+      cy.visit('https://example.cypress.io/')
+    })
+  
     it('verify that by clicking "type" navigates to a new url', function() {
-      cy.visit('https://example.cypress.io')
-      
       cy.contains('type').click()
 
       cy.url().should('include', '/commands/actions')
     })
-  })
   
+    it('verify the window title', () => {
+      cy.title().should('include', 'Kitchen Sink')
+    })
+  })
